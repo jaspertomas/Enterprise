1337891b0d534f3f83cbfb802b1cb4ef1dbc8180
Jasper Tomas <jaspertomas@gmail.com>
2014-01-15 16:10:43 +0800

    Sha1Helper working ok

---
+++ EnterpriseClient/src/utils/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author jaspertomas
+ */
+public class Test {
+        public static void main(String[] args) throws NoSuchAlgorithmException {
+        System.out.println(Sha1Helper.sha1("hello"));
+    }
+}
